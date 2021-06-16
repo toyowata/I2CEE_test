fb4a4ae65c724d7e14e7657d6b2fbd1b79b7cb5a
toyowata <toyomasa.watarai@gmail.com>
2021-06-16 17:55:08 +0900

    Fix erase and program size

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "I2CEEBlockDevice.h"
 // Create EEPROM device on I2C bus with 32kbytes of memory
 I2CEEBlockDevice i2cee(D14, D15, 0xa0, 32*1024);
+char buf[20] = {0};
+
 int main() {
     printf("i2cee test\n");
     // Initialize the device and print the memory layout
@@ -12,6 +14,7 @@ int main() {
     printf("i2cee program size: %llu\n", i2cee.get_program_size());
     printf("i2cee erase size: %llu\n",   i2cee.get_erase_size());
     // Write "Hello World!" to the first block
+#if 0
     char *buffer = (char*)malloc(i2cee.get_erase_size());
     sprintf(buffer, "Hello World!\n");
     i2cee.erase(0, i2cee.get_erase_size());
@@ -19,6 +22,19 @@ int main() {
     // Read back what was stored
     i2cee.read(buffer, 0, i2cee.get_erase_size());
     printf("%s", buffer);
+#else
+    char *buffer = (char*)malloc(20);
+    sprintf(buffer, "Hello World!\n");
+    if (i2cee.erase(0, strlen(buffer)+1) != BD_ERROR_OK)
+        printf("Erase error\n");
+    if (i2cee.program(buffer, 0, strlen(buffer)+1) != BD_ERROR_OK)
+        printf("Program error\n");
+    // Read back what was stored
+    if (i2cee.read(buf, 0, sizeof(buf)) != BD_ERROR_OK)
+        printf("Read error\n");
+    printf("%s", buf);
+#endif
+
     // Deinitialize the device
     i2cee.deinit();
 }
